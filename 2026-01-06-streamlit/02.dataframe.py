8fb95b1c1fa97a3e84e8644a5b93f9a14c09f9c0
rigizer <rigizer@gmail.com>
2026-01-06 09:34:43 +0900

    streamlit을 이용한 pandas 데이터프레임 출력

---
+++ 2026-01-06-streamlit/02.dataframe.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    'First Column': [1, 2, 3, 4], 
+    'Second Column': [10, 20, 30, 40]
+})
+
+st.write("### Here's our first attempt at using dataframes in Streamlit:")
+st.write(df)
